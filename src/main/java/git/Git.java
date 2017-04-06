2532effbdfa952f8582383e8ef3d8737d61c97bb
ad091192 <ad091192@C32720.ens.uqam.ca>
2017-04-06 20:06:20 -0300

    Après Maven - ajustements

--- src/main/java/git/Git.java
+++ src/main/java/git/Git.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.java.git;
+package git;
 
 /**
  *
@@ -13,6 +13,7 @@ package main.java.git;
 public class Git {
 
     public static void main(String[] args) {
+        
         System.out.println("Commit après passage à Maven");
         System.out.println("!!!");
         
